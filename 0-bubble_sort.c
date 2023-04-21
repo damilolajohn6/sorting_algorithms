f1f93cd1c115a5afc064c5375ea2852cdb19b68b
damilolajohn6 <damilolajohn622@gmail.com>
2023-04-21 10:45:14 -0700

    first push

---
+++ 0-bubble_sort.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+/**
+ * bubble_sort - function that sorts numbers
+ *
+ * @array: pointer to array that contains nums to be sorted
+ * @size: size of the array passed to the function
+ *
+ * Return: 0
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	int temp;
+
+	if (array == NULL)
+		return;
+
+	for (size_t i = 0; i < size - 1; i++)
+	{
+		for (size_t j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
